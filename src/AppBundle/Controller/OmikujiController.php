9f666e053ba3c055c4fe22a8db1b6a891cf1535f
KD044 <kidkd044@gmail.com>
2021-02-03 01:42:57 +0900

    First commit

---
+++ src/AppBundle/Controller/OmikujiController.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;   // (a)
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route; // (b)
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\Unsei;                // (a)
+use AppBundle\Repository\UnseiRepository;  // (b)
+
+class OmikujiController extends Controller  // ①
+{
+    /**
+     * おみくじ運勢を表示する
+     * 
+     * @Route("/omikuji/{yourname}",defaults={"yourname" = "YOU"}, name="omikuji")  // ②
+     * 
+     * @param Request $request
+     * @return Response
+     */
+    public function omikujiAction(Request $request,$yourname)  // ③
+    {
+        $omikuji = ['大吉', '中吉', '小吉', '末吉', '凶'];
+        $number = rand(0, count($omikuji) - 1);
+
+    /** @var UnseiRepository */
+    $repository = $this->getDoctrine()->getRepository(Unsei::class); // ①
+    $omikuji = $repository->findAll(); // ②
+
+    $number = rand(0, count($omikuji) - 1);
+        
+     return $this->render('omikuji/omikuji.html.twig', [ // ①
+        'name' => $yourname,
+        'unsei' => $omikuji[$number],
+    ]);
+    }
+    /**
+     * @Route("/find")
+     */
+    public function findAction()
+    {
+        /**
+         * エンティティの検索はリポジトリを通して行う ①
+         * @var UnseiRepository $repository 
+         */
+        $repository = $this->getDoctrine()->getRepository(Unsei::class);
+
+        // 全て検索 ②
+        $unseis = $repository->findAll();
+        dump($unseis);
+        
+        // ID(プライマリキー)で検索 ③
+        $unsei = $repository->find(1);
+        dump($unsei);
+
+        // 複数の項目で１件だけ検索（ここでは'name'だけですが...）④
+        $unsei = $repository->findOneBy([
+            'name' => '大吉',
+        ]);
+        dump($unsei);
+        
+        // 複数の項目で複数件検索（ここでは'name'だけですが...）⑤
+        // ※ 配列が返ってくる
+        $unsei = $repository->findBy([
+            'name' => '大吉',
+        ]);
+        dump($unsei);
+        
+        // プロパティに対応したダイナミックメソッドを使って１件だけ検索 ⑥
+        $unsei = $repository->findOneById(1);
+        dump($unsei);
+        $unsei = $repository->findOneByName('中吉');
+        dump($unsei);
+        
+        // ダイナミックメソッドを使って複数件検索 ⑦
+        $unsei = $repository->findByName('中吉');
+        dump($unsei);
+        
+        die; // プログラムを終了して、dumpを画面に表示 ⑧
+        return new Response("Dummy");
+    }
+
+    /**
+     * @Route("/crud")
+     */
+    public function crudAction()
+    {
+        /**
+         * エンティティの作成、更新、削除はエンティティマネージャを通して行う ①
+         *  @var EntityManager $em
+         */
+        $em = $this->getDoctrine()->getManager();
+
+        //
+        // Create
+        //
+        $unsei = new Unsei();    // ②
+        $unsei->setName("大凶");
+        dump($unsei);
+
+        $em->persist($unsei);    // ③
+        $em->flush();            // ④
+        dump($unsei);
+
+        //
+        // Read ⑤
+        //
+        $repository = $em->getRepository(Unsei::class);
+        
+        /** @var Unsei $unsei */
+        $unsei = $repository->findOneByName('大凶'); // ⑥
+        dump($unsei);
+        
+        //
+        // Update ⑦
+        //
+        $unsei->setName("大大吉");
+        $em->flush();
+        dump($unsei);
+        
+        $unsei = $repository->find($unsei->getId());
+        dump($unsei);
+        
+        // 
+        // Delete ⑧
+        // 
+        $em->remove($unsei);
+        $em->flush();
+        
+        $unseis = $repository->findAll();
+        dump($unseis);
+        foreach ($unseis as $unsei) {
+            dump($unsei->getName());
+        }
+        
+        die; // プログラムを終了して、dumpを画面に表示
+
+        return new Response("Dummy");
+    }    
+    /**
+     * @Route("/dql")
+     */
+    public function dql()
+    {
+        /** @var EntityManager $em */
+        $em = $this->getDoctrine()->getManager();
+        $query = $em->createQuery(
+            'SELECT u
+            FROM AppBundle:Unsei u
+            WHERE u.name = :name'        // ①
+        )->setParameter('name', '大吉');
+        
+        $unsei = $query->getResult();
+        dump($unsei);
+        
+        die; // プログラムを終了して、dumpを画面に表示
+
+        return new Response("Dummy");
+    }
+
+    
+
+
+}
---
+++ src/AppBundle/Controller/UnseiController.php
@@ -0,0 +1,158 @@
+<?php
+namespace AppBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use AppBundle\Entity\Unsei;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use AppBundle\Form\UnseiType;
+
+
+/**
+ * @Route("/unsei")  // ①
+ */
+class UnseiController extends Controller
+{
+    /**
+     * 運勢一覧の表示
+     * 
+     * @Route("/", name="unsei_index")
+     * @Method("GET")  // ①
+     * 
+     * @param Request $request
+     * @return Response
+     */
+    public function indexAction(Request $request)
+    {
+        $repository = $this->getDoctrine()->getRepository(Unsei::class);
+        $unseis = $repository->findAll();
+
+        return $this->render('unsei/index.html.twig', ['unseis' => $unseis]);
+    }
+    
+    /**
+     * 運勢の新規作成
+     * 
+     * @Route("/new", name="unsei_new")
+     * @Method({"GET", "POST"})  // ②
+     * 
+     * @param Request $request
+     * @return Response
+     */
+    public function newAction(Request $request)
+    {
+        $unsei = new Unsei();
+
+        // $form = $this->createFormBuilder($unsei) 
+        //     ->add('name', TextType::class) 
+        //     ->getForm(); 
+
+        $form = $this->createForm(UnseiType::class, $unsei);
+
+        // Form送信のハンドリング
+        $form->handleRequest($request);                   // ①
+        if ($form->isSubmitted() && $form->isValid()) {   // ②
+            /** @var EntityManager $em */
+            $em = $this->getDoctrine()->getManager();     // ③
+            $em->persist($unsei);
+            $em->flush();
+            
+            return $this->redirectToRoute('unsei_index'); // ④
+        }
+
+        return $this->render('unsei/new.html.twig', [ // ⑤
+            'form' => $form->createView()
+        ]);
+
+    }
+    
+    /**
+     * 運勢の編集
+     * 
+     * @Route("/{id}/edit", name="unsei_edit")
+     * @Method({"GET", "PUT"})  // ③
+     * 
+     * @param Request $request
+     * @return Response
+     */
+    public function editAction(Request $request, $id)// ルートパラメータのidを受け取る
+    {
+        // ① $idでPostを検索します。
+        $repository = $this->getDoctrine()->getRepository(Unsei::class);
+        $unsei = $repository->find($id);
+        
+        // ② Unseiが存在しない時、Exceptionを発生させます。
+        if (!$unsei) {
+            throw $this->createNotFoundException('No unsei found for id '.$id);
+        }
+        
+        $form = $this->createForm(UnseiType::class, $unsei, [ // ①
+            'method' => 'PUT',  // ②
+        ]);
+        
+        $form->handleRequest($request); // ③
+        if ($form->isSubmitted() && $form->isValid()) {
+            $em = $this->getDoctrine()->getManager();
+            $em->flush(); // ④
+            
+            return $this->redirectToRoute('unsei_index');
+        }
+
+        return $this->render('unsei/edit.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+    
+    /**
+     * 運勢の削除
+     * 
+     * @Route("/{id}", name="unsei_delete")
+     * @Method("DELETE")  // ④
+     * 
+     * @param Request $request
+     * @return Response
+     */
+    public function deleteAction(Request $request, $id)
+    {
+        $repository = $this->getDoctrine()->getRepository(Unsei::class);
+        $unsei = $repository->find($id);
+
+        if (!$unsei) {
+            throw $this->createNotFoundException('No unsei found for id '.$id);
+        }
+        
+        if ($this->isCsrfTokenValid('unsei', $request->get('_token'))) { // ②
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($unsei); // ③
+            $em->flush();
+        }
+
+        return $this->redirectToRoute('unsei_index');
+    }
+    
+
+
+    /**
+     * @Route("/validate/{name}", defaults={"name" = ""}) // ②
+     */
+    public function validateAction($name)
+    {
+        $unsei = new Unsei();   // ③
+        $unsei->setName($name);
+
+        $validator = $this->get('validator');    // ④
+        $errors = $validator->validate($unsei);  // ⑤
+
+        if (count($errors) > 0) {
+            return $this->render('unsei/validate.html.twig', [ // ⑥
+                'errors' => $errors,
+            ]);
+        }
+
+        return new Response("「{$name}」は正しい運勢です！"); // ⑦
+    }
+}
